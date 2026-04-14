fd6e33fbcbdc555f54b07171fa7c30a805849a76
eddme-root <ferreiraad3@gmail.com>
2026-04-14 10:17:52 -0400

    created files doctors, contact, about, login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 
 import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './components/Login'
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <Routes >
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='doctors' element={<Doctors />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='login' element={<Login />} />
         </Route>
 
       </Routes>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,66 +1,44 @@
 import { NavLink, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
 import {assets} from '../assets/assets'
 
 const Navbar = () => {
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
-    const [ showMenu, setShowMenu ] = useState(false)
-    const [ token, setToken ] = useState(true)
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
         
         <img 
-            src={assets.logo} 
-            alt=""
-            onClick={()=> navigate('/')}
-            className='cursor-pointer w-44 ' 
+            className='w-44 cursor-pointer'
+            src={assets.logo}
         />
 
-        <ul className="md:flex hidden items-start gap-5 font-medium">
+        <ul className="hidden md:flex items-start gap-5 font-medium">
             <NavLink to='/' >
-                <li className="py-1" >HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <li className='py-1' >HOME</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
             <NavLink to='/doctors' >
-                <li className="py-1" >ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <li className='py-1' >ALL DOCTORS</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
             <NavLink to='/about' >
-                <li className="py-1" >ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <li className='py-1' >ABOUT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
             <NavLink to='/contact' >
-                <li className="py-1" >CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <li className='py-1' >CONTACT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            
         </ul>
-
-        <div className='fex items-center gap-4'>
-            {token
-                ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-                    <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
-                    <img className='w-2.5' src={assets.dropdown_icon} alt="" />
-                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-                        <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                            <p onClick={()=> navigate('my-profile')} className='hover:text-black cursor-point'>My Profile</p>
-                            <p onClick={()=> navigate('my-appointments')} className='hover:text-black cursor-point'>My Appointments</p>
-                            <p onClick={()=> setToken(false)} className='hover:text-black cursor-point'>Logout</p>
-                        </div>
-                    </div>
-                </div>
-                : <button 
-                    onClick={()=> navigate('/login')} 
-                    className="bg-white text-gray-600 border border-gray-300 md:inline hidden text-sm hover:bg-gray-50 active:scale-95 transition-all w-40 h-11 rounded-full"
-                   >
-                    Get started
-                   </button>
-            }
+        <div>
+            <button
+                onClick={()=> navigate('/login')}
+                className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'
+            >
+                Create Account
+            </button>
         </div>
-
-
     </div>
 
   )
